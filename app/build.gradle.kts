dc3d90389a049fad51106073ee35b3f4c84f8879
Victor Jimenez <vj.jimeneznunez@gmail.com>
2024-09-22 20:41:14 -0400

    fix: set agp version to 8.1.2 in app-level build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
-    alias(libs.plugins.android.application)
+    id("com.android.application") version "8.1.2"
+    kotlin("android")
+    kotlin("kapt")
     alias(libs.plugins.jetbrains.kotlin.android)
 }
 
